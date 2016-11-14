b95bea6e483633472d88cf0773592dbc4198c668
Hai Dang Tran <htran@mpi-inf.mpg.de>
2016-11-14 18:13:02 +0100

    Change scripts

--- run-experiment.sh
+++ run-experiment.sh
@@ -1,3 +1,3 @@
 mvn compile
-export MAVEN_OPTS="-Xmx400G"
+#export MAVEN_OPTS="-Xmx400G"
 mvn exec:java -Dexec.mainClass="com.mpii.saarland.germany.experiment.Conductor" -Dexec.args="$1 $2 $3 $4"
--- run-pattern-mining.sh
+++ run-pattern-mining.sh
@@ -1,5 +1,5 @@
 mvn compile
-export MAVEN_OPTS="-Xmx400G"
+#export MAVEN_OPTS="-Xmx400G"
 if [ $# -eq 1 ]
 then
 	mvn exec:java -Dexec.mainClass="com.mpii.saarland.germany.rulemining.patternmining.PatternForm1Miner" -Dexec.args="$1"
